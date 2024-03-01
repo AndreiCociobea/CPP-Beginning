3254ca889d88148ac10d3e0903e0650d388dfc3a
AndreiCociobea <andreicociobea38@gmail.com>
2024-03-01 12:05:39 +0200

    .

---
+++ Projects/myFirstCPP/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main()
+{
+  using std::cout;
+  using std::endl;
+  using std::string;
+
+  cout << "Hello World!" << endl;
+  cout << "I want the World!" << endl;
+
+  string beer = "kozel";
+
+  cout << beer;
+
+  int number = 3;
+
+  return 0;
+}
