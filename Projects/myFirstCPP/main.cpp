5dd7ecf450f7f07a82d4bb6d2cf6b6a8232fa088
AndreiCociobea <andreicociobea38@gmail.com>
2024-03-02 12:08:45 +0200

    eh

--- Projects/myFirstCPP/main.cpp
+++ Projects/myFirstCPP/main.cpp
@@ -2,18 +2,36 @@
 
 int main()
 {
-  using std::cout;
-  using std::endl;
-  using std::string;
+  /*nt number = 3;
+  char one = 'd';
+  std::cout << one << std::endl;
 
-  cout << "Hello World!" << endl;
-  cout << "I want the World!" << endl;
+  int students = 25;
+  int classes = 5;
+  students++;
+  std::cout << students * classes << std::endl;
 
-  string beer = "kozel";
+double number1 = 1.3331345;
+double number2 = 3.666343;
+std::cout << number1+number2 << std::endl;*/
+  /* using std::cout;
+       using std::endl;
+       using std::string;
 
-  cout << beer;
+       cout << "Hello World!" << endl;
+       cout << "I want the World!" << endl;
 
-  int number = 3;
+       string beer = "kozel" ;
+       string snack = "pop-corn";
+
+       cout << beer <<endl;
+       cout << snack <<endl;*/
+   std::string name; //= "userName";
+
+   std::cout<< "what is your name?:";
+   std::cin >> name;
+
+   std::cout<<"salut " << name << std::endl;
 
   return 0;
 }
