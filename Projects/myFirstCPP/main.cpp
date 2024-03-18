e8b76a7d681a2be7c0cd68c138b8256c767a76e1
AndreiCociobea <andreicociobea38@gmail.com>
2024-03-18 18:33:35 +0200

    f

--- Projects/myFirstCPP/main.cpp
+++ Projects/myFirstCPP/main.cpp
@@ -1,37 +1,23 @@
 #include <iostream>
-
+//something isn t right here 
 int main()
 {
-  /*nt number = 3;
-  char one = 'd';
-  std::cout << one << std::endl;
+  
+  std::string str = std:: cin >> str ;
 
-  int students = 25;
-  int classes = 5;
-  students++;
-  std::cout << students * classes << std::endl;
+  int num1 = 4;
+  int num2 = 5;
 
-double number1 = 1.3331345;
-double number2 = 3.666343;
-std::cout << number1+number2 << std::endl;*/
-  /* using std::cout;
-       using std::endl;
-       using std::string;
+  num1=num2;
 
-       cout << "Hello World!" << endl;
-       cout << "I want the World!" << endl;
-
-       string beer = "kozel" ;
-       string snack = "pop-corn";
-
-       cout << beer <<endl;
-       cout << snack <<endl;*/
-   std::string name; //= "userName";
-
-   std::cout<< "what is your name?:";
-   std::cin >> name;
-
-   std::cout<<"salut " << name << std::endl;
+  
+  
+  if (num1 == 5)
+  {
+    
+    std:: cout << str << std::endl;
+  }
+  
 
   return 0;
 }
